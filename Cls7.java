fb4e945199decd606764d243e11d336fb1c981f2
Torunima Das <theturna10@gmail.com>
2025-08-24 16:39:20 +0600

    Create Cls7.java

---
+++ Cls7.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt(); // number of test cases
+
+        for (int i = 0; i < T; i++) {
+            try {
+                long n = scanner.nextLong();
+                System.out.println(n + " can be fitted in:");
+                if (n >= Byte.MIN_VALUE && n <= Byte.MAX_VALUE) {
+                    System.out.println("* byte");
+                }
+                if (n >= Short.MIN_VALUE && n <= Short.MAX_VALUE) {
+                    System.out.println("* short");
+                }
+                if (n >= Integer.MIN_VALUE && n <= Integer.MAX_VALUE) {
+                    System.out.println("* int");
+                }
+                if (n >= Long.MIN_VALUE && n <= Long.MAX_VALUE) {
+                    System.out.println("* long");
+                }
+            } catch (Exception e) {
+                // If the number is too big for long
+                String input = scanner.next();
+                System.out.println(input + " can't be fitted anywhere.");
+            }
+        }
+
+        scanner.close();
+    }
+}
